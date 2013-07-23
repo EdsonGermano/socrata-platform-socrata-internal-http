933bbf9cda61becfadd31a689b9eebae38601e61
Robert J. Macomber <robert.macomber@socrata.com>
2013-07-23 11:36:49 -0700

    Add AuxiliaryData for placing in a Curator payload

---
+++ src/main/scala/com/socrata/internal/http/AuxiliaryData.scala
@@ -0,0 +1,14 @@
+package com.socrata.internal.http
+
+import com.socrata.internal.http.pingpong.PingInfo
+
+/** A class for storing in a curator service advertisement's payload field.
+  * All fields of this class must be (de)serializable by Jackson. */
+class AuxiliaryData(var pingInfo: Option[PingInfo]) {
+  @deprecated(message = "This constructor is for Jackson's use, not yours", since = "forever")
+  def this() = this(None)
+
+  // can't use @BeanProperty because of the Option
+  def getPingInfo = pingInfo.orNull
+  def setPingTarget(pt: PingInfo) { pingInfo = Option(pt) }
+}
